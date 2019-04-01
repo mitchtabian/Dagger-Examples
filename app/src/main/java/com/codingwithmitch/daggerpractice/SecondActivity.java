b00281289d94402500041a12f593cac1b545fe95
Mitch Tabian <mitch@tabian.ca>
2019-04-01 13:00:04 -0700

    viewmodel injection

---
+++ app/src/main/java/com/codingwithmitch/daggerpractice/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.codingwithmitch.daggerpractice;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/codingwithmitch/daggerpractice/di/ViewModelModule.java
@@ -0,0 +1,23 @@
+package com.codingwithmitch.daggerpractice.di;
+
+import com.codingwithmitch.daggerpractice.viewmodels.MainViewModel;
+import com.codingwithmitch.daggerpractice.viewmodels.ViewModelProviderFactory;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel.class)
+    public abstract ViewModel bindMainViewModel(MainViewModel viewModel);
+
+    @Binds
+    public abstract ViewModelProvider.Factory bindViewModelFactory(ViewModelProviderFactory viewModelFactory);
+
+}
