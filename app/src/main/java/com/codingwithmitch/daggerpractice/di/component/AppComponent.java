7b20b7a9affe028fa482236e0c26435c0608d6aa
Mitch Tabian <mitch@tabian.ca>
2019-04-01 18:10:03 -0700

    room setup with repo

--- app/src/main/java/com/codingwithmitch/daggerpractice/di/component/AppComponent.java
+++ app/src/main/java/com/codingwithmitch/daggerpractice/di/component/AppComponent.java
@@ -13,8 +13,8 @@ import javax.inject.Singleton;
 import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjector;
-import dagger.android.DaggerApplication;
 import dagger.android.support.AndroidSupportInjectionModule;
+import dagger.android.support.DaggerApplication;
 
 @Singleton
 @Component(modules = {
