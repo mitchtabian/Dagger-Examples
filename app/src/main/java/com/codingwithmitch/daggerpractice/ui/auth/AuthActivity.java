73f6ab987c8384197e38540ed4a4a1e162a640cd
Mitch Tabian <mitch@tabian.ca>
2019-04-22 15:23:18 -0700

    managing authentication with dagger part 2

--- app/src/main/java/com/codingwithmitch/daggerpractice/ui/auth/AuthActivity.java
+++ app/src/main/java/com/codingwithmitch/daggerpractice/ui/auth/AuthActivity.java
@@ -59,7 +59,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
     }
 
     private void subscribeObservers(){
-        viewModel.observeUser().observe(this, new Observer<AuthResource<User>>() {
+        viewModel.observeAuthState().observe(this, new Observer<AuthResource<User>>() {
             @Override
             public void onChanged(AuthResource<User> userAuthResource) {
                 if(userAuthResource != null){
--- app/src/main/java/com/codingwithmitch/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/codingwithmitch/daggerpractice/ui/auth/AuthViewModel.java
@@ -2,6 +2,7 @@ package com.codingwithmitch.daggerpractice.ui.auth;
 
 import android.util.Log;
 
+import com.codingwithmitch.daggerpractice.SessionManager;
 import com.codingwithmitch.daggerpractice.models.User;
 import com.codingwithmitch.daggerpractice.network.auth.AuthApi;
 
@@ -20,59 +21,54 @@ public class AuthViewModel extends ViewModel {
 
     private static final String TAG = "AuthViewModel";
 
-    private final AuthApi authApi;
-
-    private MediatorLiveData<AuthResource<User>> authUser = new MediatorLiveData<>();
+    // inject
+    private final SessionManager sessionManager; // @Singleton scoped dependency
+    private final AuthApi authApi; // @AuthScope scoped dependency
 
     @Inject
-    public AuthViewModel(AuthApi authApi) {
+    public AuthViewModel(AuthApi authApi, SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         this.authApi = authApi;
         Log.d(TAG, "AuthViewModel: viewmodel is working...");
-
     }
 
-    public void authenticateWithId(int userId){
-        authUser.setValue(AuthResource.loading((User)null));
-
-        final LiveData<AuthResource<User>> source = LiveDataReactiveStreams.fromPublisher(
-
-                authApi.getUser(userId)
-
-                        // instead of calling onError (error happens)
-                        .onErrorReturn(new Function<Throwable, User>() {
-                            @Override
-                            public User apply(Throwable throwable) throws Exception {
-                                User errorUser = new User();
-                                errorUser.setId(-1);
-                                return errorUser;
-                            }
-                        })
-
-                        .map(new Function<User, AuthResource<User>>() {
-                            @Override
-                            public AuthResource<User> apply(User user) throws Exception {
-                                if(user.getId() == -1){
-                                    return AuthResource.error("Could not authenticate", (User)null);
-                                }
-                                return AuthResource.authenticated(user);
-                            }
-                        })
-
-                .subscribeOn(Schedulers.io())
-        );
-
-        authUser.addSource(source, new Observer<AuthResource<User>>() {
-            @Override
-            public void onChanged(AuthResource<User> user) {
-                authUser.setValue(user);
-                authUser.removeSource(source);
-            }
-        });
+    public LiveData<AuthResource<User>> observeAuthState(){
+        return sessionManager.getAuthUser();
     }
 
-    public LiveData<AuthResource<User>> observeUser(){
-        return authUser;
+    public void authenticateWithId(int userId) {
+        Log.d(TAG, "attemptLogin: attempting to login.");
+        sessionManager.authenticateWithId(queryUserId(userId));
     }
+
+    private LiveData<AuthResource<User>> queryUserId(int userId) {
+
+        return LiveDataReactiveStreams.fromPublisher(authApi.getUser(userId)
+
+                // instead of calling onError, do this
+                .onErrorReturn(new Function<Throwable, User>() {
+                    @Override
+                    public User apply(Throwable throwable) throws Exception {
+                        User errorUser = new User();
+                        errorUser.setId(-1);
+                        return errorUser;
+                    }
+                })
+
+                // wrap User object in AuthResource
+                .map(new Function<User, AuthResource<User>>() {
+                    @Override
+                    public AuthResource<User> apply(User user) throws Exception {
+                        if(user.getId() == -1){
+                            return AuthResource.error("Could not authenticate", null);
+                        }
+                        return AuthResource.authenticated(user);
+                    }
+                })
+                .subscribeOn(Schedulers.io()));
+    }
+
+
 }
 
 
