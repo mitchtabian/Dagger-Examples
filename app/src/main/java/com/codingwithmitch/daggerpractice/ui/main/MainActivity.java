8e4dd8aa7752619f14e133a37434c4b34ba3bdb6
Mitch Tabian <mitch@tabian.ca>
2019-04-25 10:09:48 -0700

    navigation drawer androidx

--- app/src/main/java/com/codingwithmitch/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/com/codingwithmitch/daggerpractice/ui/main/MainActivity.java
@@ -9,18 +9,26 @@ import android.widget.Toast;
 import com.codingwithmitch.daggerpractice.BaseActivity;
 import com.codingwithmitch.daggerpractice.R;
 import com.codingwithmitch.daggerpractice.ui.main.posts.PostsFragment;
-import com.codingwithmitch.daggerpractice.ui.main.profile.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         testFragment();
     }
@@ -29,6 +37,29 @@ public class MainActivity extends BaseActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.main_container, new PostsFragment())
                 .commit();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch(menuItem.getItemId()){
+
+            case R.id.nav_profile:{
+
+
+                break;
+            }
+
+            case R.id.nav_posts:{
+
+                break;
+            }
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     @Override
